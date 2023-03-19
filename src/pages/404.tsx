05de33b7d3d1392d26b5a51bbf84ff616cb9dca1
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2023-03-19 23:10:05 +0100

    feat: style 404 and about pages

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,10 @@
 const NotFound404 = () => {
-  return <h2>404</h2>;
+  return (
+    <main className="page flex flex-col items-center justify-center text-slate-200">
+      <h2 className="text-4xl">404</h2>
+      <h3 className="text-3xl">There's nothing to find here.</h3>
+    </main>
+  );
 };
 
 export default NotFound404;
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,9 @@
 const About = () => {
-  return <h2>About Us</h2>;
+  return (
+    <main className="page flex items-center justify-center text-4xl text-slate-200">
+      <h2>About Us.</h2>
+    </main>
+  );
 };
 
 export default About;
