937452c3345dd1f62fb85eca77ae97332951c7a1
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2023-03-19 23:23:38 +0100

    test: add test for Card

---
+++ src/components/Cards/Card/Card.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+
+import Card from './Card';
+
+import { character } from './character.example';
+
+describe('Card', () => {
+  it('Renders Sasuke', () => {
+    render(<Card {...character} />);
+    expect(screen.getByRole('img')).toHaveAttribute('src', character.images.webp.image_url);
+    expect(screen.getByRole('heading', { level: 2 })).toHaveTextContent('Sasuke Uchiha');
+    expect(screen.getByText('うちは サスケ')).toBeInTheDocument();
+    expect(screen.getByText('🩵 27984')).toBeInTheDocument();
+  });
+});
