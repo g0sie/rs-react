9d069472a72f1300ceb5778f47a892b4ac616227
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2023-03-20 00:07:54 +0100

    feat: style SearchBar

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -23,9 +23,15 @@ export class SearchBar extends Component {
 
   render() {
     return (
-      <form>
-        <input onChange={(e) => this.handleChange(e)} value={this.state.term}></input>
-        <button type="submit"></button>
+      <form className="rounded-3xl pl-8 py-1 bg-slate-300 xl:w-1/3 lg:w-1/2 md:w-2/3 w-3/4 flex items-center justify-between shadow-lg shadow-cyan-900">
+        <input
+          className="h-10 inline-block w-full bg-transparent text-xl focus:outline-slate-900 text-slate-900"
+          onChange={(e) => this.handleChange(e)}
+          value={this.state.term}
+        ></input>
+        <button className="px-3" type="submit">
+          🔍
+        </button>
       </form>
     );
   }
