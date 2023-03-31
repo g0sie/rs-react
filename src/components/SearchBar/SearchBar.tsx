dad47f35122d2c7c9fd6499bfac94a5542920655
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2023-03-31 12:54:21 +0200

    refactor: change SearchBar to functional component

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,65 +1,56 @@
-import { ChangeEvent, FormEvent, Component } from 'react';
+import { ChangeEvent, FormEvent, useState, useEffect } from 'react';
 
 interface SearchBarProps {
   onSearch: (term: string) => void;
 }
 
-interface SearchBarState {
-  term: string;
-}
+const SearchBar = (props: SearchBarProps) => {
+  const [term, setTerm] = useState<string>('');
 
-export class SearchBar extends Component<SearchBarProps, SearchBarState> {
-  constructor(props: SearchBarProps) {
-    super(props);
-    this.state = { term: this.getTermFromLocalStorage() };
-  }
-
-  getTermFromLocalStorage() {
+  const getTermFromLocalStorage = () => {
     const term = localStorage.getItem('term');
-    return term !== null ? term : '';
-  }
+    return term ?? '';
+  };
 
-  saveTermInLocalStorage() {
-    localStorage.setItem('term', this.state.term);
-  }
+  const saveTermInLocalStorage = (term: string) => {
+    localStorage.setItem('term', term);
+  };
 
-  handleChange(e: ChangeEvent<HTMLInputElement>) {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const newTerm = e.target.value;
-    this.setState({ term: newTerm });
-  }
+    setTerm(newTerm);
+    saveTermInLocalStorage(newTerm);
+  };
 
-  handleSubmit(e: FormEvent<HTMLFormElement>) {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    this.props.onSearch(this.state.term);
-    this.saveTermInLocalStorage();
-  }
+    props.onSearch(term);
+  };
 
-  componentWillUnmount(): void {
-    this.saveTermInLocalStorage();
-  }
+  useEffect(() => {
+    setTerm(getTermFromLocalStorage());
+  }, []);
 
-  render() {
-    return (
-      <form
-        onSubmit={(e) => this.handleSubmit(e)}
-        className={[
-          'xl:w-1/3 lg:w-1/2 md:w-2/3 w-3/4',
-          'flex items-center justify-between',
-          'rounded-3xl shadow-lg shadow-cyan-800',
-        ].join(' ')}
-      >
-        <input
-          className="inline-block h-12 w-full px-7 rounded-l-3xl bg-slate-200 text-slate-900 placeholder:text-slate-500 text-lg"
-          onChange={(e) => this.handleChange(e)}
-          value={this.state.term}
-          placeholder="Search for anime character..."
-        />
-        <button className="px-3 h-12 bg-slate-200 rounded-r-3xl" type="submit">
-          🔍
-        </button>
-      </form>
-    );
-  }
-}
+  return (
+    <form
+      onSubmit={(e) => handleSubmit(e)}
+      className={[
+        'xl:w-1/3 lg:w-1/2 md:w-2/3 w-3/4',
+        'flex items-center justify-between',
+        'rounded-3xl shadow-lg shadow-cyan-800',
+      ].join(' ')}
+    >
+      <input
+        className="inline-block h-12 w-full px-7 rounded-l-3xl bg-slate-200 text-slate-900 placeholder:text-slate-500 text-lg"
+        onChange={(e) => handleChange(e)}
+        value={term}
+        placeholder="Search for anime character..."
+      />
+      <button className="px-3 h-12 bg-slate-200 rounded-r-3xl" type="submit">
+        🔍
+      </button>
+    </form>
+  );
+};
 
 export default SearchBar;
