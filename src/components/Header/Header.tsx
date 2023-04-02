93d6c75cc78703d486b3c20c13273a9679c6dc32
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2023-04-02 19:04:17 +0200

    refactor: make Header a functional component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,29 +1,26 @@
-import { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
 import { pages } from '../../App';
 
-export class Header extends Component {
-  render() {
-    return (
-      <header className="h-16 flex items-center justify-center">
-        <nav>
-          <ul className="flex gap-8">
-            {pages.map((page, indx) => (
-              <li key={indx} className="text-slate-200 text-2xl">
-                <NavLink
-                  className={({ isActive }) => (isActive ? 'text-cyan-300' : 'hover-text-shadow')}
-                  to={page.path}
-                >
-                  {page.name}
-                </NavLink>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </header>
-    );
-  }
-}
+const Header = () => {
+  return (
+    <header className="h-16 flex items-center justify-center">
+      <nav>
+        <ul className="flex gap-8">
+          {pages.map((page, indx) => (
+            <li key={indx} className="text-slate-200 text-2xl">
+              <NavLink
+                className={({ isActive }) => (isActive ? 'text-cyan-300' : 'hover-text-shadow')}
+                to={page.path}
+              >
+                {page.name}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
