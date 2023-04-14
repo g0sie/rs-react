0b8bf162216599294a999979caefdebd7457d69a
g0sie <malgorzataderkacz3@gmail.com>
2023-04-14 09:16:12 +0200

    refactor: move Modal from Home to Cards

--- src/context/ModalContext.tsx
+++ src/context/ModalContext.tsx
@@ -2,14 +2,12 @@ import { createContext } from 'react';
 import { CharacterInterface } from '../interfaces/CharacterInterface';
 
 interface ModalContextType {
-  isOpen: boolean;
   open: (character: CharacterInterface) => void;
   close: () => void;
   character: CharacterInterface | null;
 }
 
 const ModalContext = createContext<ModalContextType>({
-  isOpen: false,
   open: (character: CharacterInterface) => {},
   close: () => {},
   character: null,
