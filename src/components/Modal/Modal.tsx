1de3c0f07774cce6ee727c57513bf9eef1ad18e0
g0sie <malgorzataderkacz3@gmail.com>
2023-04-14 10:38:23 +0200

    feat: add close button to Modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -5,6 +5,7 @@ import { motion } from 'framer-motion';
 
 import Backdrop from '../Backdrop/Backdrop';
 import HeartIcon from '../../ui/HeartIcon';
+import CloseIcon from '../../ui/CloseIcon';
 
 import './Modal.css';
 
@@ -42,25 +43,30 @@ const Modal = () => {
         animate="visible"
         exit="exit"
       >
-        <section className="modal-left">
-          <img src={modal.character?.images.webp.image_url} />
-          <h2 className="text-slate-200 text-2xl sm:pt-5 pt-1">{modal.character?.name}</h2>
-          <p className="text-slate-400 text-base">{modal.character?.name_kanji}</p>
-          <p className="text-cyan-300 text-lg flex gap-1.5">
-            <HeartIcon width="1.2rem" />
-            <span className="pt-0.5">{modal.character?.favorites}</span>
-          </p>
-        </section>
-        <section className="modal-right">
-          {modal.character?.about?.split('\n').map((p, indx) => (
-            <p
-              className="text-slate-400 pb-1 text-base"
-              key={`c-${modal.character?.mal_id}-p-${indx}`}
-            >
-              {p}
+        <div className="modal__close-btn text-slate-400 hover:text-slate-200" onClick={modal.close}>
+          <CloseIcon width="1.5rem" />
+        </div>
+        <div className="modal__content">
+          <section className="modal-left">
+            <img src={modal.character?.images.webp.image_url} />
+            <h2 className="text-slate-200 text-2xl sm:pt-5 pt-1">{modal.character?.name}</h2>
+            <p className="text-slate-400 text-base">{modal.character?.name_kanji}</p>
+            <p className="text-cyan-300 text-lg flex gap-1.5">
+              <HeartIcon width="1.2rem" />
+              <span className="pt-0.5">{modal.character?.favorites}</span>
             </p>
-          ))}
-        </section>
+          </section>
+          <section className="modal-right">
+            {modal.character?.about?.split('\n').map((p, indx) => (
+              <p
+                className="text-slate-400 pb-1 text-base"
+                key={`c-${modal.character?.mal_id}-p-${indx}`}
+              >
+                {p}
+              </p>
+            ))}
+          </section>
+        </div>
       </motion.div>
     </Backdrop>
   );
