de4ec026a9b539b6257c33cd57e9fad888150f66
g0sie <malgorzataderkacz3@gmail.com>
2023-04-16 22:42:45 +0200

    test: add searchOnMount prop test for SearchBar

--- src/components/SearchBar/SearchBar.test.tsx
+++ src/components/SearchBar/SearchBar.test.tsx
@@ -1,9 +1,9 @@
 import { fireEvent, screen } from '@testing-library/react';
-import { renderWithProvider } from '../../utils/renderWithProvider';
-
 import { vi } from 'vitest';
 
 import SearchBar from './SearchBar';
+
+import { renderWithProvider } from '../../utils/renderWithProvider';
 
 describe('SearchBar', () => {
   it('Searches on input', () => {
@@ -17,5 +17,18 @@ describe('SearchBar', () => {
     expect(searchBar).toHaveValue('naruto');
     expect(mockedSearch).toBeCalledWith('naruto');
     expect(mockedSearch).toHaveBeenCalledTimes(1);
+  });
+
+  describe('searchOnMount', () => {
+    it('When true, searches on mount', () => {
+      const mockedSearch = vi.fn();
+      renderWithProvider(<SearchBar handleSearch={mockedSearch} searchOnMount={true} />);
+      expect(mockedSearch).toHaveBeenCalledTimes(1);
+    });
+    it("When false, doesn't search on mount", () => {
+      const mockedSearch = vi.fn();
+      renderWithProvider(<SearchBar handleSearch={mockedSearch} searchOnMount={false} />);
+      expect(mockedSearch).toHaveBeenCalledTimes(0);
+    });
   });
 });
