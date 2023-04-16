65b1ea3035e61a3d98a212942101de95d5ae3daf
g0sie <malgorzataderkacz3@gmail.com>
2023-04-16 16:51:30 +0200

    refactor: fetch characters using RTK Query

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,32 +1,30 @@
-import { ChangeEvent, FormEvent } from 'react';
+import { ChangeEvent, FormEvent, useEffect, useRef, useState } from 'react';
 
 import { useSelector, useDispatch } from 'react-redux';
 import { selectSearchedTerm } from '../../pages/Home/searchSlice';
-import { setTerm } from '../../pages/Home/searchSlice';
+import { setTerm as setStoredTerm } from '../../pages/Home/searchSlice';
 
-import useMountEffect from '../../hooks/useMountEffect';
-
-interface SearchBarProps {
-  handleSearch: (term: string) => void;
-}
-
-const SearchBar = (props: SearchBarProps) => {
+const SearchBar = () => {
+  const storedTerm = useSelector(selectSearchedTerm);
+  const term = useRef<string>(storedTerm);
   const dispatch = useDispatch();
-  const term = useSelector(selectSearchedTerm);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const newTerm = e.target.value;
-    dispatch(setTerm(newTerm));
+    term.current = newTerm;
   };
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    dispatch(setStoredTerm(term.current));
     e.preventDefault();
-    props.handleSearch(term);
   };
 
-  useMountEffect(() => {
-    props.handleSearch(term);
-  });
+  const saveTermOnUnmount = () => {
+    return () => {
+      dispatch(setStoredTerm(term.current));
+    };
+  };
+  useEffect(saveTermOnUnmount, []);
 
   return (
     <form
@@ -40,7 +38,7 @@ const SearchBar = (props: SearchBarProps) => {
       <input
         className="inline-block h-12 w-full px-7 rounded-l-3xl bg-slate-200 text-slate-900 placeholder:text-slate-500 text-lg"
         onChange={(e) => handleChange(e)}
-        value={term}
+        defaultValue={storedTerm}
         placeholder="Search for anime character..."
       />
       <button className="px-3 h-12 bg-slate-200 rounded-r-3xl" type="submit">
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,31 +1,30 @@
-import { useState } from 'react';
-import { useBoolean } from 'usehooks-ts';
+import { useEffect, useState } from 'react';
+
+import { useSelector } from 'react-redux';
+import { selectSearchedTerm } from './searchSlice';
 
 import SearchBar from '../../components/SearchBar/SearchBar';
 import Cards from '../../components/Cards/Cards';
 
 import { CharacterInterface } from '../../interfaces/CharacterInterface';
 
-import axios from '../../api/axios';
 import Loader from '../../ui/Loader/Loader';
+import { useSearchCharactersQuery } from '../../api/apiSlice';
 
 const Home = () => {
-  const [characters, setCharacters] = useState<CharacterInterface[]>([]);
-  const isLoading = useBoolean(true);
+  const term = useSelector(selectSearchedTerm);
+  const { data, isLoading } = useSearchCharactersQuery(term);
 
-  const searchCharacters = async (term: string) => {
-    isLoading.setTrue();
-    const response = await axios.get('characters', { params: { q: term } });
-    const data = JSON.parse(await response.data);
-    const characters = data.data;
-    setCharacters(characters);
-    isLoading.setFalse();
-  };
+  const [characters, setCharacters] = useState<CharacterInterface[]>([]);
+
+  useEffect(() => {
+    if (data) setCharacters(data.data);
+  }, [data]);
 
   return (
     <main className="page flex flex-col gap-10 items-center pb-12">
-      <SearchBar handleSearch={searchCharacters} />
-      {isLoading.value ? <Loader /> : <Cards characters={characters} />}
+      <SearchBar />
+      {isLoading ? <Loader /> : <Cards characters={characters} />}
     </main>
   );
 };
