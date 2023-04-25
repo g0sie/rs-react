dfc220668d57cd3821de3116b0e0d541bbce5271
g0sie <malgorzataderkacz3@gmail.com>
2023-04-25 16:07:27 +0200

    feat: add h1 to SearchBar

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -32,24 +32,28 @@ const SearchBar = (props: SearchBarProps) => {
   });
 
   return (
-    <form
-      onSubmit={(e) => handleSubmit(e)}
-      className={[
-        'xl:w-1/3 lg:w-1/2 md:w-2/3 w-3/4',
-        'flex items-center justify-between',
-        'rounded-3xl shadow-lg shadow-cyan-800',
-      ].join(' ')}
-    >
-      <input
-        className="inline-block h-12 w-full px-7 rounded-l-3xl bg-slate-200 text-slate-900 placeholder:text-slate-500 text-lg"
-        onChange={(e) => handleChange(e)}
-        value={term}
-        placeholder="Search for anime character..."
-      />
-      <button className="px-3 h-12 bg-slate-200 rounded-r-3xl" type="submit">
-        🔍
-      </button>
-    </form>
+    <>
+      <h1 className="text-3xl text-slate-200 -mb-6 text-center">Search for anime characters</h1>
+
+      <form
+        onSubmit={(e) => handleSubmit(e)}
+        className={[
+          'xl:w-1/3 lg:w-1/2 md:w-2/3 w-3/4',
+          'flex items-center justify-between',
+          'rounded-3xl shadow-lg shadow-cyan-800',
+        ].join(' ')}
+      >
+        <input
+          className="inline-block h-12 w-full px-7 rounded-l-3xl bg-slate-200 text-slate-900 placeholder:text-slate-500 text-lg"
+          onChange={(e) => handleChange(e)}
+          value={term}
+          placeholder="Rock Lee"
+        />
+        <button className="px-3 h-12 bg-slate-200 rounded-r-3xl" type="submit">
+          🔍
+        </button>
+      </form>
+    </>
   );
 };
 
